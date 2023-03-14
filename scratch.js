e11eb9e631385648ad0b2b81c14f096e4edff931
Sven Ćurković <sven.curkovic@gmail.com>
2023-03-14 10:38:28 +0000

    Initial commit.

--- scratch.js
+++ scratch.js
@@ -1,45 +1,70 @@
 import dip from 'dumpster-dip'
 
-const afwiki = `/Users/spencer/data/wikipedias/afwiki-latest-pages-articles.xml`
+const wiki = `../masters/data/raw/wikipedia/en/enwiki-latest-pages-articles.xml`
 
 const opts = {
-  input: afwiki,
+  input: wiki,
   // directory for all our new files
   outputDir: './results', // (default)
   // how we should write the results
-  outputMode: 'nested', // (default)
+  outputMode: 'ndjson', // (default)
   // which wikipedia namespaces to handle (null will do all)
   namespace: 0, //(default article namespace)
   // define how many concurrent workers to run
-  workers: 3, // default is cpu count
-  //interval to log status
   heartbeat: 5000, //every 5 seconds
 
   // what do return, for every page
   parse: function (doc) {
-    return {
-      title: doc.title(),
-      // cats: doc.categories()
+    page = {
+        "title": doc.title(),
+	"pageID": doc.pageID(),
+	"sections": []
     }
-  }, // (default)
-  // should we return anything for this page?
-  doPage: function () { return true }, // (default)
-  // add plugins to wtf_wikipedia
-  // extend: (wtf) => {
-  //   wtf.extend((models) => {
-  //     models.Doc.prototype.isPerson = function () {
-  //       return this.categories().find((cat) => cat.match(/people/))
-  //     }
-  //   })
-  // },
+    for (let section of doc.sections()) {
+	section_title = section.title();
+	section_depth = section.depth();
+
+	current_section = {
+	    "title": section_title,
+	    "depth": section_depth,
+	    "sentences": []
+	}
+
+	for (let sentence of section.sentences()) { 
+	    let sentence_json = sentence.json();
+
+	    let links = []
+	    if (sentence_json.links !== undefined) {
+		for (let link of sentence_json.links) {
+		    if (link.type === "internal") {
+			links.push({
+			    "text": link.text,
+		            "title": link.page
+			})
+		    }
+		}
+	    }
+
+
+	    current_section.sentences.push({
+                "text": sentence_json["text"],
+                "links": links,
+	    });
+	}
+	
+	if (current_section["sentences"].length > 0) {
+	    page.sections.push(current_section);
+	}
+    }
+
+    if (page.sections.length == 0) {
+        return null;
+    }
+
+    return page;
+  },
 }
 
 dip(opts).then(() => {
   console.log('done')
 })
-
-
-// import getPath from 'dumpster-dip/nested-path'
-// let file = getPath('Dennis Rodman')
-// console.log(file)
-// ./BE/EF/Dennis_Rodman.txt
--- src/worker/index.js
+++ src/worker/index.js
@@ -4,6 +4,9 @@ import wtf from 'wtf_wikipedia'
 import reader from './01-reader.js'
 import { magenta } from '../_lib.js'
 import output from '../output/index.js'
+import wtf_plugin_i18n from 'wtf-plugin-i18n'
+
+wtf.extend(wtf_plugin_i18n)
 
 let { input, outputDir, outputMode, index, workers, namespace, redirects, disambiguation } = workerData
 let methods = JSONfn.parse(workerData.methods)
